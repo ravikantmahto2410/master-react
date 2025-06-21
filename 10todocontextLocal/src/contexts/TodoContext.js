34dfced313fa43e02b0c60fa687527634f2bf25f
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-06-21 19:05:30 +0530

    Context is ready now

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -10,8 +10,14 @@ export const TodoContext = createContext({
             todo: "Todo Message", //isko aap todo bol do, todo Title bol lo, todoMsg bollo
             completed:false,
         }
-    ] //ye to hogyi properties aur next likhne padegi functionalities
+    ], //ye to hogyi properties aur next likhne padegi functionalities
+    //now we will write functionalities aur functionalities ke ander definitions nhi likhte hai sirf uske naam likhte hai
+    //forexample addTodo: (todo) => {} addTodo kaise functionalities hai kuch nhi aap ek todoYahan paass karoge(todo means message) , to  message aap jaise mujhe pass karoge to ye function kuch kaam karega, kya kaam kya karega nhi pata ye kon kaam karega ye hum decide karenge bahar decide karenge
     
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 })
 
 
